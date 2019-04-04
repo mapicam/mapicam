870ce382afe86d4b20312e9a8a6010cd16cb2ce5
ANT <velmyshanovnyi@gmail.com>
2019-04-04 13:28:41 +0300

    *

---
+++ MapiCamVideo2PhotoBlackVue-0.100.cmd
@@ -0,0 +1,7 @@
+mkdir "F:\BlackVue\Record"
+mkdir "F:\BlackVue\Record\0100"
+mapillary_tools video_process --advanced --verbose --user_name velmyshanovnyi --import_path "F:\BlackVue\Record\0100" --video_import_path "F:\BlackVue\Record" --geotag_source "blackvue_videos" --geotag_source_path "F:\BlackVue\Record" --use_gps_start_time --interpolate_directions --video_sample_interval 0.100 --device_make "Blackvue" --device_model "DR900S-1CH" --overwrite_EXIF_gps_tag --duplicate_distance 0.100
+@echo #######################
+@echo ##### END PROCESS #####
+@echo #######################
+pause
--- MapiCamVideo2PhotoBlackVue.cmd
+++
@@ -1,6 +0,0 @@
-mkdir "F:\BlackVue\Record"
-
-mapillary_tools video_process --advanced --verbose --user_name velmyshanovnyi --import_path "F:\BlackVue\Record" --video_import_path "F:\BlackVue\Record" --geotag_source "blackvue_videos" --geotag_source_path "F:\BlackVue\Record" --use_gps_start_time --interpolate_directions --video_sample_interval 0.033 --device_make "Blackvue" --device_model "DR900S-1CH" --overwrite_EXIF_gps_tag --duplicate_distance 0.033
-@echo .
-
-pause
