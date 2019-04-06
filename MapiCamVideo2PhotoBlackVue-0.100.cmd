d81d5391306240e4f3fd7b6762d3937dea9c6dc1
ANT <velmyshanovnyi@gmail.com>
2019-04-06 18:28:49 +0300

    Update MapiCamVideo2PhotoBlackVue-0.100.cmd

--- MapiCamVideo2PhotoBlackVue-0.100.cmd
+++ MapiCamVideo2PhotoBlackVue-0.100.cmd
@@ -1,7 +1,10 @@
 mkdir "F:\BlackVue\Record"
 mkdir "F:\BlackVue\Record\test\0100"
 mkdir "F:\BlackVue\Record\test\csv"
-mapillary_tools video_process --advanced --verbose --user_name velmyshanovnyi --import_path "F:\BlackVue\Record\test\0100" --video_import_path "F:\BlackVue\Record" --geotag_source "blackvue_videos" --geotag_source_path "F:\BlackVue\Record" --use_gps_start_time --interpolate_directions --offset_angle 0 --video_sample_interval 0.1 --device_make "Blackvue" --device_model "DR900S-1CH" --overwrite_EXIF_gps_tag --duplicate_distance 0.1 --csv_path "F:\BlackVue\Record\test\csv" --filename_column 1 --timestamp_column 4 --latitude_column 2 --longitude_column 3
+mapillary_tools video_process --advanced --verbose --user_name velmyshanovnyi --import_path "F:\BlackVue\Record\test\0100" --video_import_path "F:\BlackVue\Record" --geotag_source "blackvue_videos" --geotag_source_path "F:\BlackVue\Record" --use_gps_start_time --interpolate_directions --offset_angle 0 --video_sample_interval 0.1 --device_make "Blackvue" --device_model "DR900S-1CH" --overwrite_EXIF_gps_tag --duplicate_distance 0.1
+
+@echo --csv_path "F:\BlackVue\Record\test\csv" --filename_column 1 --timestamp_column 4 --latitude_column 2 --longitude_column 3
+
 @echo #######################
 @echo ##### END PROCESS #####
 @echo #######################
