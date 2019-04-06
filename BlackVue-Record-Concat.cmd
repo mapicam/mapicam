d47fdd206d7d61eb447a116e17f7d0f09a027c7d
ANT <velmyshanovnyi@gmail.com>
2019-04-06 18:56:15 +0300

    *

--- BlackVue-Record-Concat.cmd
+++ BlackVue-Record-Concat.cmd
@@ -1,8 +1,10 @@
 @echo ##### START BlackVue-Record-Concat        #####
-set concatTXT=D:\BlackVue\BlackVue-Record-ListFileMP4.txt
-set concatMP4=D:\BlackVue\BlackVue-Record-concat.mp4
-set listFileTXT=D:\BlackVue\BlackVue-Record-ListFileMP4.txt
-set listFileMP4=D:\BlackVue\Record\*.mp4
+set BlackVueDrive=F:
+set concatTXT=BlackVue\BlackVue-Record-ListFileMP4.txt
+set concatMP4=BlackVue\BlackVue-Record-concat.mp4
+set listFileTXT=BlackVue\BlackVue-Record-ListFileMP4.txt
+set listFileMP4=BlackVue\Record\*.mp4
+@echo BlackVueDrive=%BlackVueDrive%
 @echo concatTXT=%concatTXT%
 @echo concatMP4=%concatMP4%
 @echo listFileTXT=%listFileTXT%
@@ -11,7 +13,7 @@ set listFileMP4=D:\BlackVue\Record\*.mp4
 mkdir D:\BlackVue\Record
 cd D:\BlackVue
 :: FIX ERROR https://github.com/mapicam/mapicam/issues/2
-ffmpeg -f concat -safe 0 -i %concatTXT% -codec copy %concatMP4%
+ffmpeg -f concat -safe 0 -i %BlackVueDrive%\%concatTXT% -codec copy %BlackVueDrive%\%concatMP4%
 @echo ###############################################
 @echo ##### END GET BlackVue-Record-Concat      #####
 @echo ###############################################
--- BlackVue-Record-ListFileMP4.cmd
+++ BlackVue-Record-ListFileMP4.cmd
@@ -1,8 +1,10 @@
 @echo #####  START BlackVue-Record-ListFileMP4  #####
-set concatTXT=D:\BlackVue\BlackVue-Record-ListFileMP4.txt
-set concatMP4=D:\BlackVue\BlackVue-Record-concat.mp4
-set listFileTXT=D:\BlackVue\BlackVue-Record-ListFileMP4.txt
-set listFileMP4=D:\BlackVue\Record\*.mp4
+set BlackVueDrive=F:
+set concatTXT=BlackVue\BlackVue-Record-ListFileMP4.txt
+set concatMP4=BlackVue\BlackVue-Record-concat.mp4
+set listFileTXT=BlackVue\BlackVue-Record-ListFileMP4.txt
+set listFileMP4=BlackVue\Record\*.mp4
+@echo BlackVueDrive=%BlackVueDrive%
 @echo concatTXT=%concatTXT%
 @echo concatMP4=%concatMP4%
 @echo listFileTXT=%listFileTXT%
@@ -10,7 +12,7 @@ set listFileMP4=D:\BlackVue\Record\*.mp4
 @echo ---------------------
 mkdir D:\BlackVue\Record
 cd D:\BlackVue
-(for %%i in (%listFileMP4%) do @echo file '%%i') > %listFileTXT%
+(for %%i in (%BlackVueDrive%\%listFileMP4%) do @echo file '%%i') > %BlackVueDrive%\%listFileTXT%
 @echo ###############################################
 @echo ##### END GET BlackVue-Record-ListFileMP4 #####
 @echo ###############################################
