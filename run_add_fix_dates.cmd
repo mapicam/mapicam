16be0750fd29ae3bbbb8324bb6409814c6dc3ae5
ANT <velmyshanovnyi@gmail.com>
2019-04-08 21:41:03 +0300

    *

--- run_add_fix_dates.cmd
+++ run_add_fix_dates.cmd
@@ -1,2 +1,4 @@
 REM <...>\python27\python <десь>\mapillary_tools\python\add_fix_dates.py <каталог з фото>\ "2019-03-27 14:07:08"
 C:\Python27\python.exe D:\mapicam\tools\mapillary\mapillary_tools\python\add_fix_dates.py F:\BlackVue\Record\0100\ "2019-12-31 23:59:59"
+REM (c) Mykhaylo Solodzhuk
+
