e3a6ce4c0199a806c2df85f093064be4a8921e34
ANT <velmyshanovnyi@gmail.com>
2019-04-08 08:10:31 +0300

    *

--- BlackVue-MoveJPG.cmd
+++ BlackVue-MoveJPG.cmd
@@ -19,34 +19,6 @@
 :: https://stackoverflow.com/questions/38554131/merge-all-gpx-files-within-a-folder-into-one-file-with-gpsbabel 
 :: 
 @echo #####################
-@echo #  YYYYMMDD HHMMSS  #
-@echo #####################
-set year=%date:~-4%
-set month=%date:~3,2%
-  if "%month:~0,1%" == " " set month=0%month:~1,1%
-set day=%date:~0,2%
-  if "%day:~0,1%" == " " set day=0%day:~1,1%
-set hour=%time:~0,2%
-  if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
-set min=%time:~3,2%
-  if "%min:~0,1%" == " " set min=0%min:~1,1%
-set secs=%time:~6,2%
-  if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
-set datetimefull=%year%-%month%-%day%T%hour%:%min%:%secs%Z
-set MapiCamDate=%year%%month%%day%
-set MapiCamTime=%hour%%min%%secs%
-@echo .
-@echo year         = %year%
-@echo month        = %month%
-@echo day          = %day%
-@echo hour         = %hour%
-@echo min          = %min%
-@echo secs         = %secs%
-@echo datetimefull = %datetimefull%
-@echo MapiCamDate  = %MapiCamDate%
-@echo MapiCamTime  = %MapiCamTime%
-@echo.
-@echo #####################
 @echo # This is parametrs #
 @echo #####################
 setlocal enabledelayedexpansion
@@ -65,12 +37,21 @@ mkdir "%BlackVue%\Record\%BlackVueFPS%fps"
 mkdir "%BlackVue%\Record\%BlackVueFPS%fps\mapillary_sampled_video_frames"
 @echo.
 
+
+pause
+ 
+(for /f "delims=" %%a in ('dir /b /s %BlackVue%\Record\%BlackVueFPS%fps\mapillary_sampled_video_frames') do @echo %%a) >> "%BlackVue%\Record\jpg\list.txt"
+
+pause
+pause
+pause
+
 :: Перенос ВСІХ файлів (.JPG) в окрему папку
 :: це в межах підготовки до зміни дати та прошивки
 :: http://forum.oszone.net/thread-214001.html
 set t=*.jpg
 set c=%BlackVue%\Record\jpg
-dir /b /s %BlackVue%\Record\%BlackVueFPS%fps\mapillary_sampled_video_frames\*\>>%t%
+dir /b /s %BlackVue%\Record\%BlackVueFPS%fps\mapillary_sampled_video_frames >> %t%
 ::dir /b /s y:\>>%t%
 ::dir /b /s z:\>>%t%
 del /F /Q "%c%\*.*" 2>nul
