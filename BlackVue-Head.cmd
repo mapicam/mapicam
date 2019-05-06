a52e83889445ee62c5891a40552896ae4b23b1e3
ANT <velmyshanovnyi@gmail.com>
2019-05-06 10:59:44 +0300

    Create BlackVue-Head.cmd

---
+++ BlackVue-Head.cmd
@@ -0,0 +1,205 @@
+@echo ##### HEAD LOAD START ###################################################
+::
+:: ###################################################
+:: #                                                 #
+:: #                    MapiCam                      #
+:: #                                                 #
+:: ###################################################
+:: #                                                 #
+:: #  www.t.me/osmUA                                 #
+:: #  @velmyshanovnyi                                #
+:: #                                         (c)2019 #
+:: #                                                 #
+:: ###################################################
+::
+:: ###################################################
+:: #                                                 #
+:: #                    ReadMe                       #
+:: #                                                 #
+:: ###################################################
+::
+:: ###################################################
+:: #                                                 #
+:: # https://t.me/osmUA                              #
+:: # https://t.me/MapillaryUkraine                   #
+:: # https://t.me/MapillaryUkraineChat               #
+:: # https://github.com/mapicam/mapicam              #
+:: # https://github.com/mapicam/mapicam/wiki         #
+:: #                                                 #
+:: ###################################################
+:: 
+cd %1%
+@set MapiCamPhaseNum=%MapiCamPhaseNum%
+@set MapiCamLOG=mapicam-LOG.txt
+@echo %date% %time% #                                                                                   >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% ############################################################### >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum%                                                                 >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # START : %MapiCamPhaseNum%                                     >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [1] START : BlackVue-Record-ListFileMP4                       >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [2] START : BlackVue-Record-Concat                            >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [3] START : BlackVueVideo2Photo                               >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [4] START : MergeGPX                                          >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [5] START : MoveJPG                                           >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [6] START : FixDateTime                                       >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [7] START : Gpx2Exif                                          >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [8] START : Upload2Mapillary                                  >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [8] START : Upload2Mapillary ReRUN                            >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% # [9] START : CleanMapillaryCache                               >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum%                                                                 >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% --------------------------------------------------------------- >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum%                                                                 >> %MapiCamLOG%
+@echo.
+@echo ####################################################
+@echo #                                                  #
+@echo # START : %MapiCamPhaseNum%                                      #
+@echo #                                                  #
+@echo ####################################################
+@echo.
+
+@echo.
+@echo #-------------------------------------------------------------#
+@echo #  YYYYMMDD HHMMSS                                            #
+@echo #-------------------------------------------------------------#
+@echo OFF
+set year=%date:~-4%
+set month=%date:~3,2%
+  if "%month:~0,1%" == " " set month=0%month:~1,1%
+set day=%date:~0,2%
+  if "%day:~0,1%" == " " set day=0%day:~1,1%
+set hour=%time:~0,2%
+  if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+set min=%time:~3,2%
+  if "%min:~0,1%" == " " set min=0%min:~1,1%
+set secs=%time:~6,2%
+  if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+set msec=%time::=%
+  set msec=%msec:~-2%
+set datetimefull=%year%-%month%-%day%T%hour%:%min%:%secs%Z
+set MapiCamDate=%year%%month%%day%
+set MapiCamTime=%hour%%min%%secs%
+@echo ON
+@echo #                                                             #
+@echo # date         = %date%                                   #
+@echo # time         = %time%                                  #
+@echo # year         = %year%                                         #
+@echo # month        = %month%                                           #
+@echo # day          = %day%                                           #
+@echo # hour         = %hour%                                           #
+@echo # min          = %min%                                           #
+@echo # secs         = %secs%                                           #
+@echo # msec         = %msec%                                           #
+@echo # datetimefull = %datetimefull%                         #
+@echo # MapiCamDate  = %MapiCamDate%                                     #
+@echo # MapiCamTime  = %MapiCamTime%                                       #
+@echo #                                                             #
+@echo #-------------------------------------------------------------#
+@echo #  YYYYMMDD HHMMSS (END)                                      #
+@echo #-------------------------------------------------------------#
+@echo.
+
+setlocal enableextensions enabledelayedexpansion
+::   MapiCamFolder=D:\mapicam
+@set MapiCamFolder=D:\mapicam
+::   version 0.4.2 - TRUE // version 0.5.0 - FALSE // 
+::   MapiCamMapillaryTools=D:\mapicam\tools\mapillary\mapillary_tools.exe
+::   MapiCamMapillaryTools=D:\mapicam\tools\mapillary\mapillary_tools-050.exe
+@set MapiCamMapillaryTools=D:\mapicam\tools\mapillary\mapillary_tools-050.exe
+:: Використовуємо механіку, коли ЯРЛИК (LNK) зчитує розташування і СКРИПТ працює відносно папки з якої запустили LNK
+::   BlackVueFolder=F:\BlackVue
+@set BlackVueFolder=%1%
+::   BlackVueFPS=10
+@set BlackVueFPS=%2%
+::   BlackVueInterval 0.1
+@set BlackVueInterval=0.1
+@IF %BlackVueFPS%==30    ( set BlackVueInterval=0.033 )
+@IF %BlackVueFPS%==10    ( set BlackVueInterval=0.1   )
+@IF %BlackVueFPS%==5     ( set BlackVueInterval=0.2   )
+@IF %BlackVueFPS%==2     ( set BlackVueInterval=0.5   )
+@IF %BlackVueFPS%==1     ( set BlackVueInterval=1     )
+@IF %BlackVueFPS%==0.5   ( set BlackVueInterval=2     )
+@IF %BlackVueFPS%==0.2   ( set BlackVueInterval=5     )
+@IF %BlackVueFPS%==0.1   ( set BlackVueInterval=10    )
+@IF %BlackVueFPS%==0.033 ( set BlackVueInterval=30    ) 
+::   --offset_angle 0
+@set BlackVueOffsetAngle=%3%
+::   --duplicate_distance 0.2
+@set BlackVueDuplicateDistance=%4%
+::   --user_name velmyshanovnyi
+@set MapiCamUsernameAtMapillary=%5%
+@set MapiCamLOG=%BlackVueFolder%\mapicam-LOG.txt
+::   --import_path "Record\jpg"
+::                  Record\jpg\.mapillary (там же має лежати папка з файлами мапілларі)
+@set uploadImportPath=Record\jpg
+@echo ---------------------------------------------------
+@echo %MapiCamPhaseNum% MapiCamFolder              = %MapiCamFolder%
+@echo %MapiCamPhaseNum% MapiCamMapillaryTools      = %MapiCamMapillaryTools%
+@echo %MapiCamPhaseNum% BlackVueFolder             = %BlackVueFolder%
+@echo %MapiCamPhaseNum% BlackVueFPS                = %BlackVueFPS%
+@echo %MapiCamPhaseNum% BlackVueInterval           = %BlackVueInterval%
+@echo %MapiCamPhaseNum% BlackVueOffsetAngle        = %BlackVueOffsetAngle%
+@echo %MapiCamPhaseNum% BlackVueDuplicateDistance  = %BlackVueDuplicateDistance%
+@echo %MapiCamPhaseNum% MapiCamUsernameAtMapillary = %MapiCamUsernameAtMapillary%
+@echo %MapiCamPhaseNum% MapiCamPhaseNum            = %MapiCamPhaseNum%
+@echo %MapiCamPhaseNum% MapiCamLOG                 = %MapiCamLOG%
+@echo %MapiCamPhaseNum% uploadImportPath           = %uploadImportPath%
+@echo %MapiCamPhaseNum%                            = %BlackVueFolder%\%uploadImportPath%
+@echo.
+@echo %date% %time% # %MapiCamPhaseNum% MapiCamFolder              = %MapiCamFolder%                    >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% MapiCamMapillaryTools      = %MapiCamMapillaryTools%            >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% BlackVueFolder             = %BlackVueFolder%                   >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% BlackVueFPS                = %BlackVueFPS%                      >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% BlackVueInterval           = %BlackVueInterval%                 >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% BlackVueOffsetAngle        = %BlackVueOffsetAngle%              >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% BlackVueDuplicateDistance  = %BlackVueDuplicateDistance%        >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% MapiCamUsernameAtMapillary = %MapiCamUsernameAtMapillary%       >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% MapiCamPhaseNum            = %MapiCamPhaseNum%                  >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% MapiCamLOG                 = %MapiCamLOG%                       >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% uploadImportPath           = %uploadImportPath%                 >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% BlackVueFolder\uploadImportPath = %BlackVueFolder%\%uploadImportPath% >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum%                                                                 >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum% --------------------------------------------------------------- >> %MapiCamLOG%
+@echo %date% %time% # %MapiCamPhaseNum%                                                                 >> %MapiCamLOG%
+@echo ---------------------------------------------------
+RMDIR %BlackVueFolder%\%MapiCamPhaseNum%-TRUE
+MKDIR %BlackVueFolder%\%MapiCamPhaseNum%-PROCESSED
+@echo ---------------------------------------------------
+@set MapiCamFFpath=c:\ffmpeg\bin
+@set MapiCamImgFolder=%BlackVueFolder%\Record\jpg
+@set MapiCamGpxFolder=%BlackVueFolder%\Record\gpx
+@set MapiCamImgDIR=00
+@set MapiCamImgDIR=%2%
+@set MapiCamHead=0
+@set MapiCamHeadXX=%MapiCamHead%
+@set offsetAngle=%MapiCamHeadXX%
+@set MapiCamInterpolationPy="D:\mapicam_tools\mapillary\mapillary_tools-master\mapillary_tools\interpolation.py"
+@set MapiCamGeotagFromGpxPy="D:\mapicam\tools\mapillary\mapillary_tools\python\geotag_from_gpx.py"
+@set MapiCamMapillaryTools="D:\mapicam\tools\mapillary\mapillary_tools.exe"
+@set MapiCamExifToolPath=D:\mapicam\tools\exiftool
+@set MapiCamExifTool=%MapiCamExifToolPath%\exiftool.exe
+@set /a MapiCamExifToolFixLocalTimeHH=3
+@set /a MapiCamExifToolFixLocalTimeMM=0
+@set /a MapiCamExifToolFixLocalTimeSS=0.000
+@echo ---------------------------------------------------
+@echo setlocal EnableDelayedExpansion
+@echo MapiCamFFpath              = %MapiCamFFpath%
+@echo MapiCamImgFolder           = %MapiCamImgFolder%
+@echo MapiCamGpxFolder           = %MapiCamGpxFolder%
+@echo MapiCamImgDIR              = %MapiCamImgDIR% (default)
+@echo BlackVueFolder             = %BlackVueFolder%
+@echo BlackVueFPS                = %BlackVueFPS%
+@echo MapiCamImgDrive            = %MapiCamImgDrive%
+@echo MapiCamHeadXX              = %MapiCamHeadXX%
+@echo offsetAngle                = %offsetAngle%
+@echo MapiCamInterpolationPy     = %MapiCamInterpolationPy%
+@echo MapiCamGeotagFromGpxPy     = %MapiCamGeotagFromGpxPy%
+@echo MapiCamMapillaryTools      = %MapiCamMapillaryTools%
+@echo MapiCamExifToolPath        = %MapiCamExifToolPath%
+@echo MapiCamExifTool            = %MapiCamExifTool%
+@echo MapiCamExifToolFixLocalTimeHH = %MapiCamExifToolFixLocalTimeHH%
+@echo MapiCamExifToolFixLocalTimeMM = %MapiCamExifToolFixLocalTimeMM%
+@echo MapiCamExifToolFixLocalTimeSS = %MapiCamExifToolFixLocalTimeSS%
+@echo ---------------------------------------------------
+
+mkdir %BlackVueFolder%\Record
+:: НЕ СТАВИТИ ПАУЗУ - бо НЕ БУДЕ працювати пакетна обробка!
+@echo ##### HEAD LOAD END #####################################################
