91537f0debd3fd54f664f17de7990833cc84c562
ANT <velmyshanovnyi@gmail.com>
2019-05-22 19:26:12 +0300

    Update BlackVue-ExifTool-Edition.cmd

--- BlackVue-ExifTool-Edition.cmd
+++ BlackVue-ExifTool-Edition.cmd
@@ -777,7 +777,7 @@ set /a "fixTime9Sec=%fixTime8Sec%"
 :: 
 :: 
 :: КОРЕГУЄМО ЗДВИГ ЧАСУ (після прошивки координат, для відновлення співпадіння з часом який на відео)
-%MapiCamExifTool% "-DateTimeOriginal+=0:0:0 0:0:%fixTime9Sec%+" "%BlackVueFolder%\Record_Call\jpg" -overwrite_original
+%MapiCamExifTool% "-DateTimeOriginal+=0:0:0 0:0:10800-%fixTime7Sec%" "%BlackVueFolder%\Record_Call\jpg" -overwrite_original
 ::%MapiCamExifTool% "-DateTimeOriginal+=0:0:0 0:0:10800.000" "%BlackVueFolder%\Record_Call\jpg" -overwrite_original
 %MapiCamExifTool% -r "-FileName<DateTimeOriginal" -d "%%Y%%m%%d-%%H%%M%%S%%%%-.1c.%%%%e" "%BlackVueFolder%\Record_Call\jpg" -overwrite_original
 :: 
