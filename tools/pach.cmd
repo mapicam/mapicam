9346caf80d3d2605655527939400713ae4040c57
ANT <velmyshanovnyi@gmail.com>
2019-06-02 19:49:45 +0300

    Create pach.cmd

---
+++ tools/pach.cmd
@@ -0,0 +1,7 @@
+:: додати до системного шляху наступні папки:
+:: -----------------------------------------
+setx /M PATH "c:\ffmpeg\bin;%PATH%"
+setx /M PATH "D:\mapicam\tools\csv2xlsx;%PATH%"
+setx /M PATH "D:\mapicam\tools\exiftool;%PATH%"
+setx /M PATH "D:\mapicam\tools\ffmpeg\bin;%PATH%"
+setx /M PATH "D:\mapicam\tools\mapillary;%PATH%"
